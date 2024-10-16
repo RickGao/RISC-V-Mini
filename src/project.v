2bedf23416a83de1e69304f6c563692646320be6
RickGao <rickgao6@outlook.com>
2024-10-16 03:55:10 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -21,9 +21,6 @@ module tt_um_koggestone_adder (
 
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
 
   wire[3:0] a, b;
   assign a = ui_in[3:0];
